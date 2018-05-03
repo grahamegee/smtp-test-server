ebc244015bfe57f9b094ee422c969c1c5b7a805b
grahamegee <grahamegee@gmail.com>
2018-05-03 11:37:51 +0100

    SMTP server with an auth handler

---
+++ server.py
@@ -0,0 +1,34 @@
+import asyncio
+import logging
+import ssl
+
+from aiosmtpd.controller import Controller
+from aiosmtpd.handlers import Sink, Debugging
+from aiosmtpd.smtp import SMTP
+
+
+class ExtendedSMTP(SMTP):
+    async def smtp_AUTH(self, arg):
+        import ipdb; ipdb.set_trace()
+
+class AuthHandler:
+    async def handle_AUTH(self, server, session, envelope):
+        import ipdb; ipdb.set_trace()
+        
+
+if __name__ == '__main__':
+    logging.basicConfig(level=logging.DEBUG)
+    
+    context = ssl.create_default_context(ssl.Purpose.CLIENT_AUTH)
+    context.load_cert_chain('cert.pem', 'key.pem')
+    
+    loop = asyncio.get_event_loop()
+    smtp = lambda: ExtendedSMTP(AuthHandler(), enable_SMTPUTF8=True, loop=loop)
+
+    server = loop.run_until_complete(
+        loop.create_server(
+            smtp,
+            host='0.0.0.0',
+            port=8025,
+            ssl=context))
+    loop.run_forever()
